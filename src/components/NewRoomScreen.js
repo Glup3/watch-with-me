567c96c0b62c9576e575d62a8357503f1a81ab2a
Glup3 <glupstar312@gmail.com>
2019-01-12 19:03:24 +0100

    add random room name generator button

--- src/components/NewRoomScreen.js
+++ src/components/NewRoomScreen.js
@@ -19,17 +19,35 @@ class NewRoomScreen extends Component {
     this.props.onSubmit(this.state.username, this.state.room);
   }
 
+  handleRoomNameClick = (e) => {
+    this.setState({
+      room: this.createRandomRoomName()
+    })
+  }
+
+  createRandomRoomName = () => {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 16; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+
   render() {
     return (
       <Row>
         <h2 className="white-text">Create or join a room</h2>
         <form onSubmit={this.handleSubmit}>
-          <Input className="white-text" placeholder="" s={12} label="Your username" required onChange={this.handleChange} id="username"/>
-          <Input className="white-text" placeholder="" s={12} label="Room ID" required onChange={this.handleChange} id="room"/>
+          <Input className="white-text" placeholder="" s={12} label="Your username" value={this.state.username} required onChange={this.handleChange} id="username"/>
+          <Input className="white-text" placeholder="" s={12} label="Room ID" required value={this.state.room} onChange={this.handleChange} id="room"/>
           <Col>
             <Button className="black">Submit</Button>
           </Col>
         </form>
+        <Button className="black" onClick={this.handleRoomNameClick}>Random room name</Button>
       </Row>
     )
   }
