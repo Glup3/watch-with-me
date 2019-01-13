6531e8adc9d663669df3420bc55c2c1549e04366
Glup3 <glupstar312@gmail.com>
2019-01-13 16:53:01 +0100

    add build

---
+++ build/precache-manifest.6c46a1eccb5470b1dab20a0aa5c8f4ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df8abac65befc554582b",
+    "url": "/static/js/main.df8abac6.chunk.js"
+  },
+  {
+    "revision": "80e6f1597631e45c09f2",
+    "url": "/static/js/1.80e6f159.chunk.js"
+  },
+  {
+    "revision": "df8abac65befc554582b",
+    "url": "/static/css/main.b59b89b3.chunk.css"
+  },
+  {
+    "revision": "edaed2bde8f16fe82d3a8f6370af2e9f",
+    "url": "/index.html"
+  }
+];
