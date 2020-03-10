b8fac5b5aa9b43e8aa04aee5f458fb5ca4cd0d21
Phuc Tran <glup3.tran@gmail.com>
2020-03-10 09:44:43 +0100

    deploy again

---
+++ build/precache-manifest.0017c4aa0430f18cf00cdbed0eb7f5f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c6738ccbbfb4a0c1e3e",
+    "url": "/static/css/main.e94117c5.chunk.css"
+  },
+  {
+    "revision": "8c6738ccbbfb4a0c1e3e",
+    "url": "/static/js/main.8c6738cc.chunk.js"
+  },
+  {
+    "revision": "290261ceee32b4b9d81c",
+    "url": "/static/js/1.290261ce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61eff50f6f525da2d907ca069f3709e0",
+    "url": "/index.html"
+  }
+];
